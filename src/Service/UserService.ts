cec4192149e4956568918f29e75ae6f822913bf6
João <jm.andrevaz@gmail.com>
2026-03-18 17:23:58 -0300

    Commit att

--- src/Service/UserService.ts
+++ src/Service/UserService.ts
@@ -3,18 +3,28 @@ import { PrismaClient } from '@prisma/client';
 const prisma = new PrismaClient();
 
 export class UserService {
-    async create(nome: string, email: string, senha: string, role: 'USER') {
+    async create(nome: string, email: string, senha: string, role: 'USER' | 'ADMIN') { // Adicionado 'ADMIN'
         const exists = await prisma.user.findUnique({ where: { email } });
         if (exists) throw new Error("Email já cadastrado.");
 
         return prisma.user.create({
-            data: { nome, email, password: senha, roleSistema: role }
+            data: { 
+                nome, 
+                email, 
+                password: senha, 
+                roleSistema: role 
+            }
         });
     }
 
     async listAll() {
         return prisma.user.findMany({
-            select: { id: true, nome: true, email: true, roleSistema: true }
+            select: { 
+                id: true, 
+                nome: true, 
+                email: true, 
+                roleSistema: true 
+            }
         });
     }
 }
